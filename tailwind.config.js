f39ff5e38fb258b41903288e49385d65f01b39b2
femnix <femnitality@gmail.com>
2025-05-30 19:06:54 +0700

    Finished with KHS. Added scroll and also feature to pick options.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
-// tailwind.config.js
 module.exports = {
   content: [
     "./*.html",
     "./src/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-daisyui: {
-  themes: [
-    {
-      light: {
-        ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-        "base-100": "#ffffff", // background for dropdowns, cards
-        "base-200": "#f2f2f2",
-        "base-content": "#1f2937", // dark text
+    extend: {
+      backgroundImage: {
+        'score-gradient': 'linear-gradient(to right, #AB9081, #937977, #7C616C, #AB5B4B)',
       },
     },
-  ],
-},
+  },
+  plugins: [require("daisyui")],
 
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          "base-100": "#ffffff",
+          "base-200": "#f2f2f2",
+          "base-content": "#1f2937",
+        },
+      },
+    ],
+  },
 }
